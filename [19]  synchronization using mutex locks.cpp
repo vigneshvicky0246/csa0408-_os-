e313ef0dea64232be8d47b6fb5fb3d83aa9875b1
vignesh. R <120434547+vigneshvicky0246@users.noreply.github.com>
2024-03-28 12:58:45 +0530

    Add files via upload

---
+++ [19]  synchronization using mutex locks.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <pthread.h>
+
+// Shared variables 
+int counter = 0;
+pthread_mutex_t mutex;
+
+// Function to be executed by threads 
+void *threadFunction(void *arg) {
+    int i;
+    for (i = 0; i < 1000000; ++i) {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_mutex_init(&mutex, NULL);
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, threadFunction, NULL);
+    pthread_create(&thread2, NULL, threadFunction, NULL);
+
+    // Wait for the threads to finish 
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    // Destroy the mutex 
+    pthread_mutex_destroy(&mutex);
+
+    // Print the final value of the counter 
+    printf("Final counter value: %d\n", counter);
+
+    return 0;
+}
+
